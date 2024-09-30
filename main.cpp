8a58c7cc11e8660eceabcbb7217b6823617677a7
tobiasrodriguezwilson <126425219+tobiasrodriguezwilson@users.noreply.github.com>
2024-09-30 13:12:29 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,64 @@
+/*! \mainpage Ejercicio Titulo
+ * \date 01/01/2023
+ * \author Nombre
+ * \section genDesc Descripcion general
+ * [Complete aqui con su descripcion]
+ *
+ * \section desarrollos Observaciones generales
+ * [Complete aqui con sus observaciones]
+ *
+ * \section changelog Registro de cambios
+ *
+ * |   Fecha    | Descripcion                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 01/01/2023 | Creacion del documento                         |
+ *
+ */
+
+/* Includes ------------------------------------------------------------------*/ 
+#include <iostream>
+#include <string>
+/* END Includes --------------------------------------------------------------*/
+
+using namespace std;
+
+/* typedef -------------------------------------------------------------------*/
+
+/* END typedef ---------------------------------------------------------------*/
+
+
+/* define --------------------------------------------------------------------*/
+
+/* END define ----------------------------------------------------------------*/
+
+
+/* Function prototypes -------------------------------------------------------*/
+
+/* END Function prototypes ---------------------------------------------------*/
+
+
+/* Global variables ----------------------------------------------------------*/
+
+/* END Global variables ------------------------------------------------------*/
+
+
+/* Function prototypes user code ----------------------------------------------*/
+
+/* END Function prototypes user code ------------------------------------------*/
+
+
+int main()
+{
+/* Local variables -----------------------------------------------------------*/
+
+/* END Local variables -------------------------------------------------------*/
+
+
+/* User code -----------------------------------------------------------------*/
+
+    cout << "Hello world!" << endl;
+
+/* END User code -------------------------------------------------------------*/
+
+    return 0;
+}
